abd5a4f090b64303205470dc174f7311c2572b70
Lucas-Alipio <lucasantonio@gec.inatel.br>
2022-07-01 17:01:26 -0300

    :sparkles: feat: Forecast+CSS

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 //import UilReact from "@iconscout/react-unicons/icons/uil-react";
+import Forecast from "./components/Forecast";
 import Inputs from "./components/Inputs";
 import TemperatureAndDetails from "./components/TemperatureAndDetails";
 import TimeAndLocation from "./components/TimeAndLocation";
@@ -6,12 +7,16 @@ import TopButtons from "./components/TopButtons";
 
 function App() {
   return (
-    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit
-    shadow-xl shadow-gray-400">
+    <div
+      className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit
+    shadow-xl shadow-gray-400"
+    >
       <TopButtons />
       <Inputs />
       <TimeAndLocation />
       <TemperatureAndDetails />
+      <Forecast title="hourly forecast" />
+      <Forecast title="daily forecast" />
     </div>
   );
 }
